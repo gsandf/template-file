b13d922d5ad1e221017e8f6f3905659cfae65754
Blake Knight <bknight@gsandf.com>
2017-07-31 11:02:17 -0500

    :sparkles: render deeply nested object values
    Resolves #1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const pify = require('pify')
 const readFile = pify(require('fs').readFile)
+const valueForProperty = require('dot-prop').get
 
 function renderString(template, data) {
   return template.replace(/\{\{\s?(.*?)\s?\}\}/g, (match, captured) => {
-    const replacement = data[captured.trim()]
+    const replacement = valueForProperty(data, captured.trim())
 
     // If a template variable is found but nothing is supplied to fill it, remove it
     if (replacement == null) {
